271eeccf0b10027213bb7dd8deace5ec10eb31d5
Aditya Khare <aditya.khare@techmonk.io>
2024-01-12 18:17:08 +0530

    FIX: dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,33 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+import preact from "@astrojs/preact";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import { remarkReadingTime } from "./src/reading-time.mjs";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.thedevguy.in",
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
-  integrations: [
-    tailwind(),
-    sitemap(),
-    mdx({
-      rehypePlugins: [
-        rehypeSlug,
-        rehypeAutolinkHeadings,
-        [
-          rehypeAutolinkHeadings,
-          {
-            behaviour: "wrap",
-          },
-        ],
-      ],
-    }),
-    react(),
-  ],
+  integrations: [tailwind(), sitemap(), mdx({
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: "wrap",
+      content: node => [{
+        type: 'element',
+        tagName: 'span',
+        children: [{
+          type: 'text',
+          value: node.children[0].value
+        }]
+      }]
+    }]]
+  }), preact()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
