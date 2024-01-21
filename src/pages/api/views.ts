5fb042c7a29468f94c0d72cf89f1cd779ad70f15
root <root@LAPTOP-7QI04CIC>
2024-01-21 21:00:18 +0530

    FIX: server build

--- src/pages/api/views.ts
+++ src/pages/api/views.ts
@@ -1,64 +1,64 @@
-import type { APIRoute } from "astro";
-import {
-  collection,
-  getDocs,
-  getDoc,
-  setDoc,
-  doc,
-  query,
-  where,
-} from "firebase/firestore";
+// import type { APIRoute } from "astro";
+// import {
+//   collection,
+//   getDocs,
+//   getDoc,
+//   setDoc,
+//   doc,
+//   query,
+//   where,
+// } from "firebase/firestore";
 
-import { db } from "../../firebase";
+// import { db } from "../../firebase";
 
-export const POST: APIRoute = async ({ request }) => {
-  const { slug, url } = await request.json();
+// export const POST: APIRoute = async ({ request }) => {
+//   const { slug, url } = await request.json();
 
-  try {
-    await createView(slug, url);
-    const q = query(collection(db, "views"), where("article", "==", url));
-    const snapshot = await getDocs(q);
-    const count = snapshot.size;
-    return new Response(
-      JSON.stringify({
-        message: "Success",
-        countUpdated: true,
-        count,
-      }),
-      { status: 200 }
-    );
-  } catch (error) {
-    return new Response(
-      JSON.stringify({
-        message: (error instanceof Error && error?.message) ?? "Failure",
-        countUpdated: false,
-      }),
-      { status: 500 }
-    );
-  }
-};
+//   try {
+//     await createView(slug, url);
+//     const q = query(collection(db, "views"), where("article", "==", url));
+//     const snapshot = await getDocs(q);
+//     const count = snapshot.size;
+//     return new Response(
+//       JSON.stringify({
+//         message: "Success",
+//         countUpdated: true,
+//         count,
+//       }),
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     return new Response(
+//       JSON.stringify({
+//         message: (error instanceof Error && error?.message) ?? "Failure",
+//         countUpdated: false,
+//       }),
+//       { status: 500 }
+//     );
+//   }
+// };
 
-async function createView(slug: string, url: string) {
-  try {
-    const response = await fetch("https://api.ipify.org?format=json");
-    const data = await response.json();
-    const user_ip: string = data.ip || "";
-    const docId = `${user_ip}-${slug}`; // Combine user IP and article URL to form a unique ID
+// async function createView(slug: string, url: string) {
+//   try {
+//     const response = await fetch("https://api.ipify.org?format=json");
+//     const data = await response.json();
+//     const user_ip: string = data.ip || "";
+//     const docId = `${user_ip}-${slug}`; // Combine user IP and article URL to form a unique ID
 
-    const docRef = doc(db, "views", docId);
-    const docSnap = await getDoc(docRef);
+//     const docRef = doc(db, "views", docId);
+//     const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      console.log("This user has been viewed this post");
-    } else {
-      if (typeof user_ip === "string") {
-        await setDoc(docRef, {
-          user_ip,
-          article: url,
-        });
-      }
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     if (docSnap.exists()) {
+//       console.log("This user has been viewed this post");
+//     } else {
+//       if (typeof user_ip === "string") {
+//         await setDoc(docRef, {
+//           user_ip,
+//           article: url,
+//         });
+//       }
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import { SITE_TITLE, SITE_DESCRIPTION } from "../constants";
 import rss from "@astrojs/rss";
-import { pagesGlobToRssItems } from "@astrojs/rss";
+// import { pagesGlobToRssItems } from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
@@ -9,7 +9,7 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.mdx")),
+    // items: await pagesGlobToRssItems(import.meta.glob("./**/*.mdx")),
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
