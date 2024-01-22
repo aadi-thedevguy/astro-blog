36e47e186f936c468720c361e2f4c4cc2e375111
Aditya Khare <adikhare24@gmail.com>
2024-01-22 08:56:06 +0530

    FIX: vercel server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import { remarkReadingTime } from "./src/reading-time.mjs";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,6 +28,6 @@ export default defineConfig({
       }]
     }]]
   }), preact()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel()
 });
